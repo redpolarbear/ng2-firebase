5bdcf05c56952eed07073f2d99017c588d610b0e
redpolarbear <matthewxu@outlook.com>
2017-03-29 15:05:05 -0700

    Add Book Stack Component

---
+++ src/app/book-stack/book-stack.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-stack',
+  templateUrl: './book-stack.component.html',
+  styleUrls: ['./book-stack.component.css']
+})
+export class BookStackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
