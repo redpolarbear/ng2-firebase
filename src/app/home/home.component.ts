c04d1d1aa930178909f1f4c1bf0623fe3a85ae23
redpolarbear <matthewxu@outlook.com>
2017-04-07 18:21:07 -0700

    home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+
+import { User } from '../auth/user';
+import { AuthService } from '../providers/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  users: Array<User> = [];
+  currentUser: any;
 
-  constructor() { }
+  constructor(private authService: AuthService) { 
+
+  }
 
   ngOnInit() {
+    // console.log(firebase.auth().currentUser);
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        console.log(user.uid);
+      } else {
+        // No user is signed in.
+      }
+    });
+    
+    firebase.database().ref('users').once('value')
+      .then( snap => {
+        // this.users = snap.val();
+        this.users = Object.keys(snap.val()).map( key => snap.val()[key]);
+        // console.log(this.users);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+
+    
   }
+
+
 
 }
